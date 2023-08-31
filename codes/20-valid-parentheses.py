8868084466d7bf8990e7844139427b9c98741705
João Guilherme Mendes <68798128+joaoguilhermemendes@users.noreply.github.com>
2023-08-30 23:33:49 -0300

    Update 20-valid-parentheses.py

--- codes/20-valid-parentheses.py
+++ codes/20-valid-parentheses.py
@@ -1,8 +1,24 @@
-class Solution(object):
-    def isValid(self, s):
-        cont = 0
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parentheses = 0
+        square = 0
+        curly = 0
+
         for elem in s:
-            print(s[cont])
-            if cont < len(s)-1:
-                cont+=1
-                print(s[cont])
+            match elem:
+                case "(":
+                    parentheses+=1
+                case ")":
+                    parentheses-=1
+                case "[":
+                    square+=1
+                case "]":
+                    square-=1    
+                case "{":
+                    curly+=1 
+                case "}":
+                    curly-=1 
+        
+        if parentheses!=0 or square!=0 or curly!=0:
+            return False
+        return True
